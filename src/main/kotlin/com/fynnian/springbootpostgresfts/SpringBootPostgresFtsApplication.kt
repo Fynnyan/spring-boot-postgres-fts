1d8f8971507a9affb1d445fbbfd382569dcfb001
Thomas Zbinden <thomas.zbinden@unic.com>
2022-03-28 08:26:08 +0200

    init

---
+++ src/main/kotlin/com/fynnian/springbootpostgresfts/SpringBootPostgresFtsApplication.kt
@@ -0,0 +1,11 @@
+package com.fynnian.springbootpostgresfts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPostgresFtsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPostgresFtsApplication>(*args)
+}
---
+++ src/test/kotlin/com/fynnian/springbootpostgresfts/SpringBootPostgresFtsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fynnian.springbootpostgresfts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootPostgresFtsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
