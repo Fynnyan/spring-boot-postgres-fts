89887e2378dad8a24635001e0c44d4bf8d4ca874
Thomas Zbinden <thomas.zbinden@unic.com>
2022-03-31 09:19:45 +0200

    updated the readme to, added example queries and http requests

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # spring-boot-postgres-fts
 demo project with spring boot, jooq and postgres full text search
 
-UNDER CONSTRUCTION
+
+## Setup
+
+- start the DB - ``docker compose up -d``
+- build the project - ``./mvnw verify``
+
+The DB is accessible under localhost:4242
+- user: fts-ftw
+- PW: fts-ftw
+- DB & schema: fts-ftw
+
+The DB contains some example data, the European CPV codes in D/F/I/E.
+
+The spring app runs under the default localhost:8080
+and exposes the following endpoints:
+
+```http request
+# treverst the CPV code tree 
+
+### get root nodes
+GET http://localhost:8080/api/cpv-codes
+
+### get next lvl, set the id of the parent as query
+GET http://localhost:8080/api/cpv-codes?parent=ID
+
+# search thru the codes 
+
+### Use the FTS search over all language will use the `simple` postgres config
+GET http://localhost:8080/api/cpv-codes/search?query=TXTX
+
+### Use the FTS search over all language will use the postgres config for the given language
+GET http://localhost:8080/api/cpv-codes/search?query=TXTX&language=en
+```
+  
+UNDER CONSTRUCTION$
 
 ![UNDER CONSTRUCTION](https://media.giphy.com/media/ZTans30ONaaIM/giphy.gif)
---
+++ example-fts-queries.sql
@@ -0,0 +1,57 @@
+
+-- what does to_tsvector do and why the language config is important
+select 'system_default' as config, to_tsvector('Sphinx of black quartz judge my vow')
+union
+select 'simple' as config, to_tsvector('simple', 'Sphinx of black quartz judge my vow')
+union
+select 'english' as config, to_tsvector('english', 'Sphinx of black quartz judge my vow')
+union
+select 'german' as config, to_tsvector('german', 'Sphinx of black quartz judge my vow')
+order by config desc;
+
+-- same as previous with german text to display the behaviour of the config i.e. looks for other stop words
+select 'system_default' as config, to_tsvector('Falsches Üben von Xylophonmusik quält jeden größeren Zwerg')
+union
+select 'simple' as config, to_tsvector('simple', 'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg')
+union
+select 'english' as config, to_tsvector('english', 'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg')
+union
+select 'german' as config, to_tsvector('german', 'Falsches Üben von Xylophonmusik quält jeden größeren Zwerg')
+order by config desc;
+
+-- what does to_tsquery do and why the language config is important
+select 'system_default' as config, to_tsquery('black & quartz | of')
+union
+select 'simple' as config, to_tsquery('simple', 'black & quartz | of')
+union
+select 'english' as config, to_tsquery('english', 'black & quartz | of')
+union
+select 'german' as config, to_tsquery('german', 'black & quartz | of')
+order by config desc;
+
+-- postgres got some convenient functions to turn input into a query string (also sanitizes the string)
+select 'system_default' as config, websearch_to_tsquery('judge my vow')
+union
+select 'simple' as config, websearch_to_tsquery('simple', 'judge my vow')
+union
+select 'english' as config, websearch_to_tsquery('english', 'judge my vow')
+union
+select 'german' as config, websearch_to_tsquery('german', 'judge my vow')
+union
+select '' as config, websearch_to_tsquery('german', 'judge, jury & executioner (my words are final!)')
+order by config desc;
+
+-- both combined to do a search
+select '1 query' as setup, to_tsquery('black & quartz')::text
+union
+select '2 vector' as setup, to_tsvector('Sphinx of black quartz judge my vow')::text
+union
+select '3 result' as setup, (to_tsvector('Sphinx of black quartz judge my vow') @@ to_tsquery('black & quartz'))::text
+order by setup;
+
+select '1 query' as setup, to_tsquery('sphinx <-> quartz')::text
+union
+select '2 vector' as setup, to_tsvector('Sphinx of black quartz judge my vow')::text
+union
+select '3 result' as setup, (to_tsvector('Sphinx of black quartz judge my vow') @@ to_tsquery('sphinx <-> quartz'))::text
+order by setup;
